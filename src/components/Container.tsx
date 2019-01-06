ce539c69c884241253448e5236e116cfbc4f893d
Robin Malhotra <me@rmalhotra.com>
2019-01-06 15:13:44 +0530

    init

---
+++ src/components/Container.tsx
@@ -0,0 +1,31 @@
+import * as React from 'react'
+import styled from '@emotion/styled'
+
+import { widths } from '../styles/variables'
+import { getEmSize } from '../styles/mixins'
+
+const StyledContainer = styled.div`
+  position: relative;
+  margin-left: auto;
+  margin-right: auto;
+  width: auto;
+  max-width: ${getEmSize(widths.lg)}em;
+`
+
+interface AppLinkProps {
+  appStoreURL: string
+}
+
+const AppLink: React.SFC<AppLinkProps> = ({ appStoreURL }) => (
+  <a href={appStoreURL} >Linkety Link</a>
+)
+
+interface ContainerProps {
+  className?: string
+}
+
+const Container: React.SFC<ContainerProps> = ({ children, className }) => (
+  <StyledContainer className={className}>{children}</StyledContainer>
+)
+
+export { Container, AppLink, StyledContainer }
